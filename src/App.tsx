14479564502aca313129a65e613c18baac2db431
eight_y_88 <jadec.eight@gmail.com>
2021-10-16 19:49:21 +0900

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+// @ts-ignore
+import {HashRouter, Route, Switch} from "react-router-dom";
+import {AnimatePresence} from 'framer-motion';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="MenuBar">
+                <img src={"avaritia-icon.png"} width={100} height={100} alt="ICON"/>
+                <h1>Modded<br/>マイクラ学園</h1>
+            </div>
+            <AnimatePresence exitBeforeEnter>
+                <HashRouter>
+                </HashRouter>
+            </AnimatePresence>
+        </div>
+    );
 }
 
 export default App;
