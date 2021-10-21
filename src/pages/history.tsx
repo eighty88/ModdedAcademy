3b7e81488972f9fa22bda320dee3d01a1b5d6f20
eight_y_88 <jadec.eight@gmail.com>
2021-10-21 20:04:34 +0900

    Update

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -26,40 +26,31 @@ export default function History() {
                 }}>
                     学園の歴史
                 </h1>
-                <dl>
+                <div>
                     {(() => {
                         // @ts-ignore
                         const items = [];
 
                         Histories.forEach(
-                            item => {
-                                items.push(<>
-                                    <dt>
-                                        { item.date.toString().substring(0, 4) }年{ item.date.toString().substring(4, 6) }月
-                                    </dt>
-                                    <dd>
-                                        <h2> { item.title } </h2>
-                                        <p> { item.text.split('\n').map((str, index) => (<React.Fragment key={index}><br />{str}</React.Fragment>))} </p>
-                                    </dd>
-                                </>)
+                            (item) => {
+                                items.push(
+                                    <dl>
+                                        <dt>
+                                            {item.date.toString().substring(0, 4)}年{item.date.toString().substring(4, 6)}月
+                                        </dt>
+                                        <dd>
+                                            <h2> {item.title} </h2>
+                                            <p> {item.text.split('\n').map((str, index) => (
+                                                <React.Fragment key={index}><br/>{str}</React.Fragment>))} </p>
+                                        </dd>
+                                    </dl>
+                                )
                             }
                         )
                         // @ts-ignore
                         return <div>{ items }</div>
                     })()}
-                </dl>
-                <dl>
-                    <dt>1919年7月</dt>
-                    <dd>
-                        <h2>創設</h2>
-                        <p>なぜこの日になってしまったのか。<br/>とりあえず日を隠していい感じにする</p>
-                    </dd>
-                    <dt>2021年??月</dt>
-                    <dd>
-                        <h2>いもとり氏学長代理就任</h2>
-                        <p>第22代目学長の逮捕に伴って<br/>立候補制で<s>適当に</s>募集をかけたところ<br/>一人だけだったため学長代理に就任<br />何書いたらいいかわからなかったのでとりあえず適当な作り話</p>
-                    </dd>
-                </dl>
+                </div>
             </motion.div>
         </Scrollbars>
     );
